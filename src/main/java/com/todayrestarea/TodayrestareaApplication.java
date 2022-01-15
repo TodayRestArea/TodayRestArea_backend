be4b42289cd661aded1868a5438095aee2ecef1a
pjoon357 <pjoon357@naver.com>
2022-01-15 16:36:54 +0900

    build

---
+++ src/main/java/com/todayrestarea/TodayrestareaApplication.java
@@ -0,0 +1,13 @@
+package com.todayrestarea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodayrestareaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodayrestareaApplication.class, args);
+	}
+
+}
