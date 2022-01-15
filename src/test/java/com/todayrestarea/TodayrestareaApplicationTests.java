c6f36285d56635ab507b925f1530c401ab8bef1b
pjoon357 <pjoon357@naver.com>
2022-01-15 17:09:52 +0900

    build

---
+++ src/test/java/com/todayrestarea/TodayrestareaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todayrestarea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodayrestareaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
